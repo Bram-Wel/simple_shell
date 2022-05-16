a54907a568aeadbc096098d2cc6e58e86e81d9d2
Bram-Wel <welbram@gmail.com>
2022-05-16 20:55:01 +0000

    Interactive shell

--- main.h
+++ main.h
@@ -5,5 +5,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+extern char **environ;
 
 char **strtok_f(char *str);
+char *_getenv(const char *name);
+void print_path_dirs(char *path);
+
+/**
+  * struct l_path - Structure storing PATH directories.
+  * @pathl: A directory in the PATH variable.
+  * @next: The next directory in the pah variable.
+  */
+typedef struct l_path
+{
+	char *pathl;
+	struct l_path *next;
+} l_path;
+
+l_path *linked_path(char *path);
+int _setenv(const char *name, const char *value, int overwrite);
--- shell.c
+++ shell.c
@@ -2,32 +2,43 @@
 
 /**
   * main - A super simple shell.
+  * @argc: Argument count.
+  * @argv:  Argument vector.
   *
   * Description: Runs Commands with their full path without
   * any argument.
   * Return: Always 0.
   */
-int main(void)
+int main(int argc, char **argv)
 {
 	ssize_t read;
 	size_t len = 0;
-	char *line = NULL, *argv[] = {"", NULL};
+	char *line = NULL;
 	pid_t child;
 	int status;
 
+	(void)argc;
 	printf("#cisfun$ ");
 	while ((read = getline(&line, &len, stdin)) != -1)
 	{
 		*(argv + 0) = line;
-		printf("Debug: %s Size: %lu Read: %lu\n", *(argv + 0), strlen(line), read);
 		*(line + (read - 1)) = '\0';
-		child = fork();
-		if (child == -1)
-			perror("Error");
-		else if (child > 0)
-			waitpid(child, &status, 0);
-		else if (execve(*(argv + 0), argv, NULL) == -1)
-			perror("Error");
+		if (strcmp(line, "exit") == 0)
+		{
+			kill(0, SIGCHLD);
+			exit(EXIT_SUCCESS);
+		}
+
+		if (*line)
+		{
+			child = fork();
+			if (child == -1)
+				perror("Error");
+			else if (child > 0)
+				waitpid(child, &status, 0);
+			else if (execve(*(argv + 0), argv, NULL) == -1)
+				perror("Error");
+		}
 		printf("#cisfun$ ");
 	}
 	printf("\n");
