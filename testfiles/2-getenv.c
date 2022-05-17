bcdbe64dd59d24a6f8aecb644e17e81a105bffc0
Bram-Wel <welbram@gmail.com>
2022-05-17 08:02:25 +0000

    Shell.

---
+++ testfiles/2-getenv.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * main - Prints an environment variable.
+  *
+  * Return: Always 0;
+  */
+int main(void)
+{
+	printf("PATH: %s\n", _getenv("PATH"));
+	printf("null: %s\n", _getenv("shit"));
+	return (0);
+}
+
+/**
+  * _getenv - Obtains an environment variable.
+  * @name: The environment varible by NAME.
+  *
+  * Return: Pointer to the environment variable.
+  */
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	int i = 0;
+	char *p = NULL;
+
+	for (; *(environ + i); i++)
+	{
+		if (**(environ + i) == *name)
+		{
+			p = *(environ + i);
+			while (*name)
+			{
+				name++;
+				p++;
+			}
+			p++;
+			return (p);
+		}
+	}
+	return (p);
+}
